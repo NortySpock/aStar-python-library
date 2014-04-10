8d3d2007460624d0304f1eafd825ee46e9057936
David Norton <david.k.norton@gmail.com>
2014-04-09 22:08:20 -0500

    apparently too much past 300x300 maps we start bumping into recursion depth problems in heapq.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import path_util as path
 import test_util as test
 
-my_cost_map = test.make_grid(100,100,1) #default cost of 1
+my_cost_map = test.make_grid(300,300,1) #default cost of 1
 #test.u_shaped_wall(my_cost_map)
 start_pos = [0,0]
-end_pos = [99,99]
+end_pos = [299,299]
 
 # my_cost_map[2][2] = 2
 #quick_list = [(3,3,3),(2,2,2),(1,1,1),(0,0,0),(-1,-1,9)]
@@ -14,11 +14,11 @@ end_pos = [99,99]
 my_text_map = test.create_text_map_from_cost_map(my_cost_map)
 my_text_map[start_pos[0]][start_pos[1]] = 'A'
 my_text_map[end_pos[0]][end_pos[1]] = 'B'
-test.pretty_print_map(my_text_map) #show board before we tamper with it
+#test.pretty_print_map(my_text_map) #show board before we tamper with it
 path_dict = path.a_star_manhattan_path(start_pos[0],start_pos[1],end_pos[0],end_pos[1], my_cost_map)
 test.print_list_of_tuples_on_map(path_dict['open'], '~',my_text_map)
 test.print_list_of_tuples_on_map(path_dict['closed'], '.',my_text_map)
 test.print_path_on_map(my_text_map,path_dict['path'])
-test.pretty_print_map(my_text_map)
+#test.pretty_print_map(my_text_map)
 test.check_path_for_validity(start_pos[0],start_pos[1],end_pos[0],end_pos[1],path_dict['path'], my_cost_map)
 
