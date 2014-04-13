4541982b48b7cdf4b33026ae5ed513db9bd9784c
David Norton <david.norton@cerner.com>
2014-04-13 18:23:15 -0500

    In competition it was found the test functions were so useful for debugging it makes more sense to just fold them into the path_util file for convenience.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import path_util as path
-import test_util as test
 
-my_cost_map = test.make_grid(40,20,1) #default cost of 1
+my_cost_map = path.make_grid(40,20,1) #default cost of 1
 
 start_pos = [10,19]
 end_pos = [10,10]
@@ -12,16 +11,16 @@ end_pos = [10,10]
 # my_cost_map[2][2] = -1
 #quick_list = [(3,3,3),(2,2,2),(1,1,1),(0,0,0),(-1,-1,9)]
 #path.apply_list_of_tuples_to_cost_map(quick_list,my_cost_map)
-test.u_shaped_wall(my_cost_map)
+path.u_shaped_wall(my_cost_map)
 
-my_text_map = test.create_text_map_from_cost_map(my_cost_map)
+my_text_map = path.create_text_map_from_cost_map(my_cost_map)
 my_text_map[start_pos[0]][start_pos[1]] = 'A'
 my_text_map[end_pos[0]][end_pos[1]] = 'B'
-test.pretty_print_map(my_text_map) #show board before we tamper with it
+path.pretty_print_map(my_text_map) #show board before we tamper with it
 path_dict = path.a_star_manhattan_path(start_pos[0],start_pos[1],end_pos[0],end_pos[1], my_cost_map)
-test.print_list_of_tuples_on_map(path_dict['open'], '~',my_text_map)
-test.print_list_of_tuples_on_map(path_dict['closed'], '.',my_text_map)
-test.print_path_on_map(my_text_map,path_dict['path'])
-test.pretty_print_map(my_text_map)
-test.check_path_for_validity(start_pos[0],start_pos[1],end_pos[0],end_pos[1],path_dict['path'], my_cost_map)
+path.print_list_of_tuples_on_map(path_dict['open'], '~',my_text_map)
+path.print_list_of_tuples_on_map(path_dict['closed'], '.',my_text_map)
+path.print_path_on_map(my_text_map,path_dict['path'])
+path.pretty_print_map(my_text_map)
+path.check_path_for_validity(start_pos[0],start_pos[1],end_pos[0],end_pos[1],path_dict['path'], my_cost_map)
 
